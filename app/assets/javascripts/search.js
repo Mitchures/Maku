e79c5c69f0ef0763234ea8f1826fec1b12785f52
Mitchell Hollander <mitchell.hollander@cybera.net>
2018-03-20 10:01:16 -0500

    styled cards with a lighter shadow

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -22,7 +22,9 @@ function handleJSON(d){
     var bp = md[i].table.backdrop_path;
     var img = "https://image.tmdb.org/t/p/w500/" + bp;
     if(bp == null) img = "http://www.saxarchitecten.nl/wp-content/uploads/2015/07/film_camera.png.625x385_q100.png";
-    var dom = "<div class='card'><img class='card-img-top img-fluid' src='" + img + "' /><div class='card-body'><h5 class='card-title'>" + md[i].table.original_title + "</h5><h6 class='card-subtitle mb-2 text-muted'>" + md[i].table.release_date + "</h6><p class='card-text'>" + md[i].table.overview + "</p></div></div>";
+    var rd = md[i].table.release_date;
+    var date = rd.substring(4, rd.indexOf('?'));
+    var dom = "<div class='card'><img class='card-img-top img-fluid' src='" + img + "' /><div class='card-body'><h5 class='card-title'>" + md[i].table.original_title + "</h5><h6 class='card-subtitle mb-2 text-muted'>" + date + "</h6><p class='card-text'>" + md[i].table.overview + "</p></div></div>";
     target.append(dom);
   }
 }
