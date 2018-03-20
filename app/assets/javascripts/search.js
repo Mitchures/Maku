fa9c32f8c193eceff48a0fdea329af49caf8b979
Mitchell Hollander <mitchell.hollander@cybera.net>
2018-03-20 09:16:40 -0500

    added card columns

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -22,7 +22,7 @@ function handleJSON(d){
     var bp = md[i].table.backdrop_path;
     var img = "https://image.tmdb.org/t/p/w500/" + bp;
     if(bp == null) img = "http://www.saxarchitecten.nl/wp-content/uploads/2015/07/film_camera.png.625x385_q100.png";
-    var dom = "<div class='col-sm'><div class='card' style='width: 500px;'><img class='card-img-top' src='" + img + "' /><div class='card-body'><h5 class='card-title'>" + md[i].table.original_title + "</h5><p class='card-text'>" + md[i].table.overview + "</p></div></div></div>";
+    var dom = "<div class='card'><img class='card-img-top img-fluid' src='" + img + "' /><div class='card-body'><h5 class='card-title'>" + md[i].table.original_title + "</h5><h6 class='card-subtitle mb-2 text-muted'>" + md[i].table.release_date + "</h6><p class='card-text'>" + md[i].table.overview + "</p></div></div>";
     target.append(dom);
   }
 }
