552cf6c0c87b1e0ef4bec14fd74c98ae378e6d97
Mitchell Hollander <MitchellSH@live.com>
2018-03-22 22:37:34 -0500

    blah

--- config/initializers/npm.rb
+++ config/initializers/npm.rb
@@ -1,5 +1,4 @@
-system 'npm install'
-# if Rails.env.development? || Rails.env.test?
+system 'npm install' if Rails.env.development? || Rails.env.test?
 # Heroku build packages
 # heroku buildpacks:add 'https://github.com/heroku/heroku-buildpack-nodejs.git'
 # heroku buildpacks:add 'https://github.com/heroku/heroku-buildpack-ruby.git'
