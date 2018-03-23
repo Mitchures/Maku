abbaaf23b48b88807f9fd78445b8e85bf065e139
Mitchell Hollander <MitchellSH@live.com>
2018-03-22 22:02:49 -0500

    deploy to heroku

--- config/initializers/npm.rb
+++ config/initializers/npm.rb
@@ -1,4 +1,4 @@
 system 'npm install' if Rails.env.development? || Rails.env.test?
 # Heroku build packages
-# heroku bulidpacks:add 'https://github.com/heroku/heroku-buildpack-nodejs.git'
-# heroku bulidpacks:add 'https://github.com/heroku/heroku-buildpack-ruby.git'
+# heroku buildpacks:add 'https://github.com/heroku/heroku-buildpack-nodejs.git'
+# heroku buildpacks:add 'https://github.com/heroku/heroku-buildpack-ruby.git'
